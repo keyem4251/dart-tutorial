7a21a1a476515697923f480333cbc6bbf62699f5
keyem4251 <kisk.emura@gmail.com>
2023-04-02 16:09:52 +0900

    control flow statements

--- introduction.dart
+++ introduction.dart
@@ -1,3 +1,32 @@
 void main() {
-  print('Hello, World');
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.4;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+
+  print(name);
+  print(antennaDiameter);
+  print(image);
 }
